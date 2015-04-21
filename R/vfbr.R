2078f6a265b73d1dee54f1a44e7f9958de7aa12f
Gregory Jefferis <jefferis@gmail.com>
2015-04-21 23:45:15 +0100

    rstudio: +roxygen

---
+++ R/vfbr.R
@@ -0,0 +1,12 @@
+#' Programmatic access to the VFB API
+#' @name vfbr-package
+#' @aliases vfbr
+#' @docType package
+#' @keywords package
+#' @import httr jsonlite
+#' @examples
+#' \dontrun{
+#' ##
+#' }
+#' @references See
+NULL
