f2bd6bbb4975f22e9f528df3170024f96833413d
Gregory Jefferis <jefferis@gmail.com>
2015-04-22 10:10:38 +0100

    clarify that a default server option

--- R/vfbr.R
+++ R/vfbr.R
@@ -2,17 +2,22 @@
 #'
 #' @section package options:
 #'
-#' You can set a default server to connect to with the option:
-#'
-#' \code{options(vfbr.server="http://vfbsandbox.inf.ed.ac.uk")}.
+#'   You can set a default server to connect to with the option
+#'   \code{vfbr.server}. This is presently set on package load as follows:
+#'   \code{options(vfbr.server="http://vfbsandbox.inf.ed.ac.uk")} unless you
+#'   have previously set it (e.g. in your \code{\link[base]{Rprofile}}) to a
+#'   different value.
 #' @name vfbr-package
 #' @aliases vfbr
 #' @docType package
 #' @keywords package
 #' @import httr jsonlite
 #' @examples
+#' # Show state of vfbr package options
+#' options()[grep('^vfbr', names(options()))]
 #' \dontrun{
-#' ##
+#' example(vfb_generic_query)
 #' }
-#' @references See
+#' @references See \url{http://virtualflybrain.org}
+#' @seealso \code{\link{vfb_generic_query}}
 NULL
