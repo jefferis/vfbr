d7be2c4f932889712887cc33a361b7f2104e7fda
Gregory Jefferis <jefferis@gmail.com>
2015-11-03 18:24:36 +0000

    Function to create stackbrowser url

---
+++ R/vfb_stack.R
@@ -0,0 +1,19 @@
+#' Make a URL for a set of VFB ids to be displayed in the stack browser
+#'
+#' @param ids A character vector of IDs
+#' @inheritParams vfb_solr_query
+#'
+#' @return character vector to open stack browser
+#' @export
+#'
+#' @examples
+#' # some gmr lines
+#' ids=c("VFBi_00004657","VFBi_00023207","VFBi_00023120","VFBi_00022264")
+#' template="VFBt_00100000"
+#' u=vfb_stack_url(c(template, ids))
+#' \dontrun{
+#' browseURL(u)
+#' }
+vfb_stack_url<-function(ids, path='/site/stacks/index.htm?', server= getOption("vfbr.server")) {
+  paste0(server, path, "/", "index.htm?add=", paste(ids, collapse = ","))
+}
