9ce5c7b762b3e8aa6d28d49641598486be5fe7ae
Gregory Jefferis <jefferis@gmail.com>
2015-11-13 11:09:39 +0000

    fix queries with character ids
    * need to json encode the params
    * see https://www.opencpu.org/api.html#api-arguments

--- R/vfb_nblast.R
+++ R/vfb_nblast.R
@@ -13,12 +13,14 @@
 #'   }
 #' @export
 #' @examples
-#' vfb_nblast('fru-M-200266', n=10)
+#' vfb_nblast('fru-M-200266')
 vfb_nblast<-function(query, n=50, ...){
   server="http://vfbdev.inf.ed.ac.uk"
   topn_path="/ocpu/library/flynblastscores/R/flycircuit_topn"
   posturl=paste0(server,topn_path)
-  r=POST(posturl, body=list(query=query, n=n, ...))
+  listbody=list(query=query, n=n, ...)
+  jsonbody=lapply(listbody, toJSON)
+  r=POST(posturl, body=jsonbody)
   stop_for_status(r)
   response_path=scan(text=content(r), n = 1, what="", sep = "\n", quiet = TRUE)
   geturl=paste0(server,response_path,"/csv")
