97dbe894637f8df2629a37a516caf231dae9996c
Gregory Jefferis <jefferis@gmail.com>
2015-11-17 15:58:11 +0000

    add vfb_3dbrowser_url
    * and a gepetto server option

--- R/vfb_stack.R
+++ R/vfb_stack.R
@@ -16,6 +16,33 @@
 #' \dontrun{
 #' browseURL(u)
 #' }
-vfb_stack_url<-function(ids, path='/site/stacks', server= getOption("vfbr.server")) {
-  paste0(server, path, "/", "index.htm?add=", paste(ids, collapse = ","))
+vfb_stack_url<-function(ids, add=TRUE, path='/site/stacks', server= getOption("vfbr.server")) {
+  paste0(server, path, "/", "index.htm?",ifelse(add,"add=","id="),
+         paste(ids, collapse = ","))
+}
+
+
+#' Make a URL for a set of VFB ids to be displayed in the 3D browser
+#'
+#' @param ids A character vector of IDs
+#' @param template VFB id of the template brain
+#' @inheritParams vfb_solr_query
+#'
+#' @return character vector to open 3D browser
+#' @export
+#'
+#' @examples
+#' # some flycircuit neurons
+#' ids=c("VFB_00004657","VFB_00023207","VFB_00023120","VFB_00022264")
+#' u=vfb_3dbrowser_url(ids)
+#'
+#' fcids=c("VGlut-F-000304", "VGlut-F-200278", "fru-F-200121", "TH-F-300016")
+#' u2=vfb_3dbrowser_url(vfb_tovfbids(fcids))
+#' \dontrun{
+#' browseURL(u)
+#' browseURL(u2)
+#' }
+vfb_3dbrowser_url<-function(ids, template="VFBt_001", server=getOption('vfbr.server.gepetto')) {
+  baseurl="%s/org.geppetto.frontend/geppetto?load_project_from_url=http://vfbdev.inf.ed.ac.uk/do/geppettoJson.json?i=%s&t=%s"
+  sprintf(baseurl, server, paste(ids, collapse = ","), template)
 }
--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 .onLoad <- function(libname, pkgname) {
   op.vfbr=list(
-    vfbr.server="http://vfbdev.inf.ed.ac.uk"
+    vfbr.server="http://vfbdev.inf.ed.ac.uk",
+    vfbr.server.gepetto="http://129.215.164.244:8084"
   )
   op<-options()
   toset <- !(names(op.vfbr) %in% names(op))
