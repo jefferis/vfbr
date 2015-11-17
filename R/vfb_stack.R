31ef53e3ce43239a597b88686dd249ef5dbaf01a
Gregory Jefferis <jefferis@gmail.com>
2015-11-17 15:57:33 +0000

    give vfb_stack_url an add arg

--- R/vfb_stack.R
+++ R/vfb_stack.R
@@ -1,6 +1,8 @@
 #' Make a URL for a set of VFB ids to be displayed in the stack browser
 #'
 #' @param ids A character vector of IDs
+#' @param add Whether to add stacks to the existing scene (the default, when
+#'   \code{add=TRUE}) or to start a new scene.
 #' @inheritParams vfb_solr_query
 #'
 #' @return character vector to open stack browser
@@ -10,7 +12,7 @@
 #' # some gmr lines
 #' ids=c("VFBi_00004657","VFBi_00023207","VFBi_00023120","VFBi_00022264")
 #' template="VFBt_00100000"
-#' u=vfb_stack_url(c(template, ids))
+#' u=vfb_stack_url(c(template, ids), add=FALSE)
 #' \dontrun{
 #' browseURL(u)
 #' }
