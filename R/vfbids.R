9784d710d935e227aef39f9de7cd0f06b147b3d3
Gregory Jefferis <jefferis@gmail.com>
2015-11-17 13:50:35 +0000

    Add vfb_fromvfbids and vfb_tovfbids
    * these are pretty generic functions that can only cope with ~ 200 specific ids
    * could make more efficient functions if one knew what wildcard pattern to use.

---
+++ R/vfbids.R
@@ -0,0 +1,51 @@
+#' Convert between VFB and external identifiers
+#'
+#' @details Note that these functions are not appropriate for fetching more than
+#'   ~ 200 identifiers individually specified identifiers. If a large number of
+#'   identifiers are required, it is a much better idea to fetch all ids and
+#'   then filter locally.
+#' @param vfbids One or more vfb identifiers. Solr wildcards can also be used
+#'   when \code{fixed=FALSE}- see examples and \code{\link{vfb_solr_query}}
+#' @param ... Additional arguments passed to \code{\link{vfb_solr_query}}.
+#' @return A character vector of vfb ids (for \code{vfb_fromvfbids}) or external
+#'   identifiers (for \code{vfb_tovfbids}) in the same order as the input query.
+#'   Missing values will be denoted by \code{NA}s when \code{fixed=TRUE}. When
+#'   \code{fixed=FALSE} and a wild-card search returns no results a character
+#'   vector of length 0 will be returned.
+#' @export
+#' @examples
+#' # some flycircuit ids
+#' fcids=c("VGlut-F-000304", "VGlut-F-200278", "fru-F-200121", "TH-F-300016")
+#' vfbids=vfb_tovfbids(fcids)
+#' vfb_fromvfbids(vfbids)
+vfb_fromvfbids<-function(vfbids, ...){
+  q=paste0("short_form", vfbids, collapse = " ")
+  rdf=vfb_solr_query(query=q, fields = "short_form+label", rows=Inf, ...)
+  if(nrow(rdf)==0) {
+    rep(NA_character_, length(vfbids))
+  } else {
+    # make sure results are in correct order with NAs for missing results
+    rdf$label[match(vfbids, rdf$short_form)]
+  }
+}
+
+#' @param ids One or more external identifiers. Solr wildcards can also be used
+#'   when \code{fixed=FALSE}- see examples and \code{\link{vfb_solr_query}}
+#' @param fixed Whether to insist on exact matches. When \code{fixed=FALSE} solr
+#'   wildcards can be used.
+#' @rdname vfb_fromvfbids
+#' @export
+vfb_tovfbids<-function(ids, fixed=TRUE, ...){
+  query_field=ifelse(fixed, "label_s:", "label:")
+  q=paste0(query_field, ids, collapse = " ")
+  rdf=vfb_solr_query(filterquery="VFB_*",query=q, fields = "short_form+label", rows = Inf, ...)
+  if(nrow(rdf)==0) {
+    if(fixed) rep(NA_character_, length(ids)) else character()
+  } else if(fixed){
+    # make sure results are in correct order with NAs for missing results
+    rdf$short_form[match(ids, rdf$label)]
+  } else {
+    # wild card results - ordering/missing values undefined
+    rdf$short_form
+  }
+}
