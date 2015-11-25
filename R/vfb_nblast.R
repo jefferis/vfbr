8a76b6288f2bcce9da840f2883e4a9b479ffbb10
Gregory Jefferis <jefferis@gmail.com>
2015-11-25 20:02:17 +0000

    switch vfb_nblast to to json encoding

--- R/vfb_nblast.R
+++ R/vfb_nblast.R
@@ -17,12 +17,10 @@
 vfb_nblast<-function(query, n=50, ...){
   server="http://vfbdev.inf.ed.ac.uk"
   topn_path="/ocpu/library/flynblastscores/R/flycircuit_topn"
-  posturl=paste0(server,topn_path)
+  posturl=paste0(server,topn_path, '/json')
   listbody=list(query=query, n=n, ...)
   jsonbody=lapply(listbody, toJSON)
   r=POST(posturl, body=jsonbody)
   stop_for_status(r)
-  response_path=scan(text=content(r), n = 1, what="", sep = "\n", quiet = TRUE)
-  geturl=paste0(server,response_path,"/csv")
-  read.csv(geturl, stringsAsFactors = FALSE)
+  jsonlite::fromJSON(content(r,as='text'))
 }
