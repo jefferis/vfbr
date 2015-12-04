ad7a4790113928b46dfd2614b9a162d324fa55ff
Gregory Jefferis <jefferis@gmail.com>
2015-12-04 20:31:35 +0000

    fix duplicate package docs
    * and document vfbr.stack.gmr_url option

--- R/vfbr-package.R
+++ R/vfbr-package.R
@@ -1,7 +1,9 @@
 #' Programmatic Access to the virtualflybrain.org website
 #'
 #' @section Package Options: The following options can be set to specify default
-#'   behaviour.
+#'   behaviour. They will be set to sensible defaults on package startup, unless
+#'   you have previously set a value (e.g. in your
+#'   \code{\link[base]{Rprofile}}).
 #'
 #'   \itemize{
 #'
@@ -10,15 +12,26 @@
 #'   \item{\code{vfbr.server.gepetto}}{ URL of VFB's gepetto server for 3D
 #'   visualisation (see \code{\link{vfb_3dbrowser_url}}.)}
 #'
+#'   \item{\code{vfbr.stack.gmr_url}}{URL containing listing of registered GMR
+#'   Gal4 confocal stacks. See \code{\link{gmr_stack_urls}}}
+#'
 #'   \item{\code{vfbr.stack.downloads}}{Location of downloaded stacks. See
 #'   \code{\link{download_gmr_stacks}}}
-
+#'
 #'   }
+#'
+#' @references See \url{http://virtualflybrain.org}
+#' @seealso \code{\link{vfb_solr_query}}
 #' @name vfbr-package
 #' @aliases vfbr
 #' @examples
 #' # Show state of vfbr package options
 #' options()[grep('^vfbr', names(options()))]
+#'
+#' \dontrun{
+#' example(vfb_solr_query)
+#' }
 #' @docType package
 #' @keywords package
+#' @import httr jsonlite
 NULL
