3261aa3a63b88272864732cd2dcd98b9702f7217
Gregory Jefferis <jefferis@gmail.com>
2016-01-11 11:40:35 +0000

    Give vfb_synonym_query a quote argument
    * this ANDs query terms

--- R/vfb_extended_query.R
+++ R/vfb_extended_query.R
@@ -1,14 +1,63 @@
-#' Find canononical value for synonyms using VFB SOLR query
+#' Find canononical ontology term using VFB SOLR to query synonyms
 #'
-#' @param x The search term. Multiple search terms are also allowed - see
-#'   details. Wildcards (\code{?*}) can be used, but
-#' @param exact Whether to do an exact match (default) or to allow partial
-#'   matches against synonyms.
+#' @param query A character vector containing one or more queries - see details.
+#'   Wildcards (\code{?*}) can be used, and have a similar meaning to their use
+#'   in the shell.
+#' @param exact Whether to do an exact match (i.e. the query must match the
+#'   whole string, the default) or to allow partial matches against synonyms.
+#'   See \bold{Query details} and examples.
+#' @param quote Whether to quote the query so that individual terms must all be
+#'   matched in the order given or to \bold{OR} each term in the query. The
+#'   default \code{quote=NA} will quote when \code{exact=TRUE} and the query
+#'   does not contain wildcards. See \bold{Query details} and \bold{Examples}.
 #' @param fields The fields to return (defaults to 'short_form label synonym').
 #'   Setting \code{fields=""} implies all fields (but note that not all results
 #'   may have the same fields - see discussion in details)
-#' @param ... Additional arguments passed to \code{\link{vfb_solr_query}}
-#' @return A data.frame containing one or more result rows or when
+#' @param verbose Whether to print messages to the screen.
+#' @param ... Additional arguments passed to \code{\link{vfb_solr_query}}. You
+#'   can use this e.g. to set the number of returned \code{rows}.
+#' @return A data.frame containing one or more result rows, ordered according to
+#'   the solr result score, with attributres
+#' @section Query details: The \code{exact} and \code{quote} arguments have a
+#'   profound effect on how SOLR carries out searches. Setting both arguments to
+#'   \code{FALSE} gives the least specific search and you may want to do this if
+#'   you do not find any results with an initial query using the default values.
+#'
+#'   In general SOLR will break any text in fields like synonym into separate
+#'   tokens based on whitespace, hypens etc, so the value :
+#'
+#'   \code{inter-antennal lobe tract}
+#'
+#'   will map onto 4 tokens.
+#'
+#'   \code{inter antennal lobe tract}
+#'
+#'   This means that a search with query \code{"inter antennal lobe tract",
+#'   exact=F} will be successful (note missing hyphen).
+#'
+#'   When \code{exact=TRUE}, the values will not be tokenised and the query must
+#'   match the whole field.
+#'
+#'   The \code{quote} argument modifies the query. By default the query is
+#'   tokenised as just described for fields in the database, tokens can be
+#'   matched in any order and only one token must match (i.e. the are
+#'   \bold{OR}ed together).  When \code{quote=TRUE}, the query must match in the
+#'   order given (this is achieved by wrapping it in double quotes before
+#'   passing to SOLR). For example
+#'
+#'   \code{vfb_synonym_query("antennal lobe tract", exact=F, quote=T)}
+#'
+#'   returns 2 results at the time of writing whereas
+#'
+#'   \code{vfb_synonym_query("antennal lobe tract", exact=F, quote=F)}
+#'
+#'   returns 194 results.
+#'
+#'   See
+#'   \url{https://github.com/EBISPOT/OLS/blob/master/ols-solr/src/main/solr-conf/ontology/conf/schema.xml}
+#'    for definition of the of the \code{synonym_s} exact match field.
+#'
+#'
 #' @details Note that data.frame that is returned may contain a list in the
 #'   synonym column because there will likely be multiple synonyms for a given
 #'   query.
@@ -20,10 +69,6 @@
 #'   matrix with rows that you can index by field name and columns that you can
 #'   index by queries. However the form of this is not guaranteed if you ask for
 #'   fields that are present only for some of the results.
-#'
-#'   See
-#'   \url{https://github.com/EBISPOT/OLS/blob/master/ols-solr/src/main/solr-conf/ontology/conf/schema.xml}
-#'    for definition of the of the synonym_s exact match field.
 #' @seealso \code{\link{vfb_solr_query}}
 #' @export
 #' @examples
@@ -31,7 +76,7 @@
 #' # anything with a synonym that includes SOG
 #' vfb_synonym_query("SOG", exact=FALSE)
 #'
-#' \donttest{#' # query for PAM exactly
+#' \donttest{# query for PAM exactly
 #' vfb_synonym_query("PAM")
 #' # any term with a synonym containing PAM
 #' vfb_synonym_query("PAM", exact = FALSE)
@@ -42,6 +87,10 @@
 #' # against tokenised strings (i.e. strings that have broken on spaces and
 #' # other non word characters such as dashes and underscores).
 #' vfb_synonym_query("PAM-*", exact = FALSE)
+#' \donttest{# However if you quote the query you will get results
+#' vfb_synonym_query("PAM-*", exact = FALSE, quote=TRUE)
+#' }
+#'
 #' \donttest{
 #' # Search for MBON-01 to MBON-22 (sprintf is used to 0 pad the numbers)
 #' vfb_synonym_query(sprintf("MBON-%02d",1:22))
@@ -54,8 +103,26 @@
 #' # wild card syntax to a regular expression
 #' mbondf$aso=sapply(mbondf$synonym, function(x) grep(glob2rx("MBON-??"), x, value=TRUE))
 #' }
-vfb_synonym_query<-function(x, exact=TRUE, fields='short_form label synonym', ...){
-  if(length(x)>1) return(sapply(x, vfb_synonym_query, exact=exact, fields=fields, ...))
+vfb_synonym_query<-function(query, exact=TRUE, quote=NA,
+                            fields='short_form label synonym',
+                            verbose=interactive(), ...){
+  if(length(query)>1) return(sapply(query, vfb_synonym_query, exact=exact, fields=fields,
+                                quote=quote, verbose=verbose, ...))
+  if(is.na(quote)){
+    # quote=F unless exact=T and query does not contain wildcards
+    quote=exact && !grepl("[*?]", query)
+  }
+  if(quote) query=sprintf('"%s"', query)
   qfield=ifelse(exact, "synonym_s", "synonym")
-  vfb_solr_query(filterquery=x, fields=fields, defaultfield = qfield, ...)
+  res=vfb_solr_query(filterquery=query, fields=fields, defaultfield = qfield, ...)
+
+  if(verbose){
+    nfound=attr(res,'numFound')
+    if(nrow(res) < nfound)
+      message("There are ", nfound, " results. ",
+              "Return them all by setting rows=Inf!")
+    if(nfound==0 && exact)
+      message("No results found! You could try setting exact=FALSE.")
+  }
+  res
 }
